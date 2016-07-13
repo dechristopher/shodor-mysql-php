499acaa974ceb3810ac8e8a9c764ce5546719dd1
Andrew DeChristopher <dechristophera@wit.edu>
2016-07-12 20:49:37 -0400

    Staged index shell

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+?>
+
+<html>
+	<head>
+		<title>Shodor Access</title>
+	</head>
+	<body>
+		<form action="#" method="post">
+			<input type="text" name="username" placeholder="Username" />
+			<input type="password" name="password" placeholder="Password" />
+			<input type="submit" name="login" value="Login" />
+		</form>
+	</body>
+</html>
